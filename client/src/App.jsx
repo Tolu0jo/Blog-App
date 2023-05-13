c0d7cc5eadb835fc66071f1461a20fe6885a5fd2
Teetony <ojotolulope@gmail.com>
2023-05-13 01:40:11 +0100

    post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/header/Header'
 import TopBar from './components/topbar/topbar'
+import Single from './pages/Single/single'
+import Write from './pages/Write/Write'
 import Home from './pages/home/Home'
 
 function App() {
   return (
     <div className='app'>
    <TopBar/>
-   <Home/>
+   {/* <Home/> */}
+   {/* <Single/> */}
+   <Write/>
   
     </div>
    
---
+++ client/src/components/singlePost/singlePost.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./singlePost.css"
+const SinglePost = () => {
+    return (
+        <div className="singlePost">
+            <div className="singlepostWrapper">
+                <img className='singlePostImage' src="https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor, sit amet consectetur
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon fa-solid fa-pen-to-square fa-xl"></i>
+                        <i className="singlePostIcon fa-solid fa-trash fa-xl"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className='singlePostAuthor'>Author : <b> John</b> </span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className='singlePostDesc'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, iusto.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default SinglePost
